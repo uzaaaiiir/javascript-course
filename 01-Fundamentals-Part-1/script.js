ffb3c07356b717015ea3364c419f4941fae43f75
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-03-09 12:11:03 -0500

    Variables and Values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JS is FUN!");
+console.log(40 + 8 + 23 - 10);
 
-let result = 40 + 8 + 23 - 10;
-console.log(result);
+console.log("Uzair");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// VARIABLE NAMING
+// -- letters, numbers, and $ sign
+// -- Cannot begin with numbers
+// -- Cannot use reserved words
+// -- Do not start with Upper Case - used for Classes
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+
+// CONSTANTS
+// -- use uppercase for constants
+let PI = 3.14159;
+
+// Variable names need to be descriptive
+let myFristJob = "Programmer";
+let myCurrentJob = "Teacher";
