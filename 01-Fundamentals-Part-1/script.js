8a9513765444e5c99980536ccfa23678772400cc
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-03-10 11:00:54 -0500

    JavaScript DataTypes

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -26,7 +27,9 @@ let PI = 3.14159;
 let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 console.log(myFirstJob);
+*/
 
+/*
 // JavaScript Fundametals -- Part 1
 // VALUES and VARIABLE -- Questions
 let country = "Canada";
@@ -34,5 +37,48 @@ let continent = "North America";
 let population = 38100000;
 
 console.log(country, continent, population);
+*/
 
-// DATA TYPES -- Questions
+/* DATATYPES
+-- Boolean is a value that is only true or false
+-- String is a sequence of characters
+-- Number is floating point numbers 
+
+typeof can be used to determine the datatype of a value stored in a variable 
+-- Bug in JS: if you use (typeof null) --> it returns object(this is a bug in JS)
+
+Dynamic Typing in JS 
+-- Can change the type of a value held in a variable
+-- done by reassigning a variable to a new value 
+*/
+
+/* // DATA TYPES CODE
+let javascriptIsFun = true;
+
+// console.log(true);
+// console.log(typeof 14);
+// console.log(typeof "Jonas");
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let isIsland = false;
+let language;
+let country = "Canada";
+let population = 34100000;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
+*/
