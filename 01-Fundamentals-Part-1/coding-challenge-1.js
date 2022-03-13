0fa2378c6dd160fbe6500df42d50b5f57385e394
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-03-13 11:12:57 -0400

    JS Coding Challenge

--- 01-Fundamentals-Part-1/coding-challenge-1.js
+++ 01-Fundamentals-Part-1/coding-challenge-1.js
@@ -17,9 +17,9 @@ const markHigherBMI = markBMI > johnBMI;
 console.log(markBMI, johnBMI, markHigherBMI);
 
 if (markHigherBMI) {
-    console.log("Mark's BMI is higher than John's");
+    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
 } else {
-    console.log("John's BMI is higherthan Mark's!");
+    console.log(`John's BMI ${johnBMI} is higherthan Mark's ${markBMI}!`);
 }
 
 // TEST DATA 2
@@ -37,7 +37,13 @@ const markHigherBMI2 = markBMI2 > johnBMI2;
 console.log(markBMI2, johnBMI2, markHigherBMI2);
 
 if (markHigherBMI2) {
-    console.log("Mark's BMI is higher than John's");
+    console.log(
+        `Mark's BMI (${Math.round(
+            markBMI2
+        )}) is higher than John's (${Math.round(johnBMI2)})!`
+    );
 } else {
-    console.log("John's BMI is higherthan Mark's!");
+    console.log(
+        `John's BMI (${johnBMI2}) is higher than Mark's (${markBMI2})!`
+    );
 }
