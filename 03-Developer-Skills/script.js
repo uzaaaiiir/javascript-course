c8ee90a9bd6aa6e30785ae40de452bdaea08faf3
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-03-26 13:24:07 -0400

    JS

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -16,3 +16,38 @@ console.log(calcAge(1999));
 console.log(13);
 
 console.log(calcAge(2005));
+
+function solution(number) {
+  let result = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      result += i;
+    }
+  }
+  return result;
+}
+
+console.log(solution(10));
+
+function descendingOrder(n) {
+  //...
+  let result = "";
+  let numberString = String(n);
+  let number = numberString.split("");
+
+  for (let i = 0; i < number.length; i++) {
+    let largest = Number(number[i]);
+    for (let j = i; j < number.length; j++) {
+      if (Number(number[j]) > largest) {
+        largest = Number(number[j]);
+      }
+    }
+
+    console.log(result);
+    result += largest;
+  }
+
+  return Number(result);
+}
+
+console.log(descendingOrder(15));
