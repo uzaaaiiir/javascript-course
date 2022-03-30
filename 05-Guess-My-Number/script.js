a878d3d96394d65d8ec78ed3b6c9de0fc976183a
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-03-30 12:30:19 -0400

    DOM Manipulation

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const messageEl = document.querySelector('.message');
+console.log(messageEl);
+console.log(messageEl.textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
