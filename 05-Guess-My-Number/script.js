05b71b95596f7d9516d5742389c39c4d7b3a3cc7
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-01 11:50:09 -0400

    Guess my Number game

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const messageEl = document.querySelector('.message');
 console.log(messageEl);
 console.log(messageEl.textContent);
@@ -11,3 +12,20 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const random = Math.trunc(Math.random() * 20) + 1;
+
+const x = function () {
+  console.log(document.querySelector('.guess').value);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); //input is returned as a string
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  }
+});
+// document.querySelector('.check').addEventListener('click', x);
