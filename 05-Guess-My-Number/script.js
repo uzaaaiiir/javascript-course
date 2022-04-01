9fd046baa6cd802bc6bf17690fcf4cdaa224254f
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-01 12:06:38 -0400

    Guess My Number Game

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -14,8 +14,11 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const random = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = random;
+//State variables - variables relevant to the state of our application
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 const x = function () {
   console.log(document.querySelector('.guess').value);
@@ -27,6 +30,16 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🙌 Correct Number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
 // document.querySelector('.check').addEventListener('click', x);
