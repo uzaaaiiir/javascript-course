fe4af75563e2417e82aa7834b910b142f424680c
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-02 16:29:13 -0400

    Modal Window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,17 +7,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 // querySelectorAll creates a Node List
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button Clicked!');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked!');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
