8a5ec1dc846ca851b05080cd4fc827aa78b84d60
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-02 16:16:27 -0400

    Modal Window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btsOpenModal = document.querySelector('.open-modal');
+
+console.log(btsOpenModal);
