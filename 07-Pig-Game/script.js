abaf6c2577827b1437f188b7ea56bbca58a914f8
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-11 01:32:41 -0400

    Pig Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,9 +3,12 @@
 const rollDice = document.querySelector('.btn--roll');
 const holdDice = document.querySelection('btn--hold');
 
+const score0El = document.querySelector('#score--0');
+//Don't need to use #
+const score1El = document.getElementById('score--1');
+
 let p1score = 0;
 let p2score = 0;
-
-function diceRoll() {}
+let number = 0;
 
 rollDice.addEventListener('click', diceRoll);
