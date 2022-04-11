6101752da540fcc8447476856f6883fb52cda65b
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-11 01:27:46 -0400

    Pig Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const rollDice = document.querySelector('.btn--roll');
+const holdDice = document.querySelection('btn--hold');
+
+let p1score = 0;
+let p2score = 0;
+
+function diceRoll() {}
+
+rollDice.addEventListener('click', diceRoll);
