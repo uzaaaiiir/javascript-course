5220bfbf5c715573bb83b49e684f6cb751ddfd46
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-11 01:36:28 -0400

    Pig Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const rollDice = document.querySelector('.btn--roll');
-const holdDice = document.querySelection('btn--hold');
-
+// Selecting Elements
 const score0El = document.querySelector('#score--0');
 //Don't need to use #
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('.hidden');
 
 let p1score = 0;
 let p2score = 0;
