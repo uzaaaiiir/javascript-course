27aad8789eea949592252d7e6ec9ffd4542daa7c
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-14 16:02:27 -0400

    Computing Scores and Game Logic

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -9,6 +9,8 @@ const diceEl = document.querySelector('.dice');
 // select players
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 // Buttons
 const btnNew = document.querySelector('.btn--new');
@@ -20,24 +22,64 @@ score1El.textContent = 0;
 // Don't need to use .hidden -- just hidden
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generate a Random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // 1. Generate a Random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1; if true switch to next player
-  if (dice !== 1) {
-    currentScore += dice;
-    current0El.textContent = currentScore; //Change later
-  } else {
-    //   switch to next player
+    // 3. Check for rolled 1; if true switch to next player
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0El.textContent = currentScore; //Change later
+    } else {
+      //   switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+// Holding Functionality
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to score of active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check if score is >= 100
+    // finish game
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to next players
+      switchPlayer();
+    }
   }
 });
