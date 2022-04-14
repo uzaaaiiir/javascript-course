f3def492f656a49e61b0f53acea944c7d95633fd
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-14 16:21:33 -0400

    Pig Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -17,15 +17,34 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-// Don't need to use .hidden -- just hidden
-diceEl.classList.add('hidden');
+// Starting conditions
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  // Don't need to use .hidden -- just hidden
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  // Remove Winner Styling
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  // Reset Styles
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+// Initial Conditions
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -69,8 +88,9 @@ btnHold.addEventListener('click', function () {
 
     // 2. check if score is >= 100
     // finish game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -83,3 +103,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// New Game Functionality
+btnNew.addEventListener('click', init);
