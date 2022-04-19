cb465dba72e451231b68191a4aba1a5f4016bf4d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-19 01:09:31 -0400

    Block Scope

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -5,8 +5,26 @@ function calcAge(birthYear) {
   //console.log(firstName); // Global variable available in function scope
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}.`;
+    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    } else {
+      millenial = false;
+      const firstName = 'John'; // separate firstName var that is block scoped
+      const str = `You're not a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str); // const and let are block scoped and only available in the block they are created
+    console.log(millenial); // var is function scoped not block scoped
+    // add(2, 3);
   }
 
   printAge();
