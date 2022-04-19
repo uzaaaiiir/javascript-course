d87010c641de25cb4c612e09d2eee1d64e73cec9
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-19 00:59:38 -0400

    Scoping

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  //console.log(firstName); // Global variable available in function scope
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}.`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Uzair';
+calcAge(1999);
+// console.log(age); // Function Scopes variable -- inaccessible outside calcAge function
