4313cdba0bf2e282754faae70a26a038a4270d99
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-19 01:38:20 -0400

    Hoisting in JS

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -35,3 +35,55 @@ function calcAge(birthYear) {
 const firstName = 'Uzair';
 calcAge(1999);
 // console.log(age); // Function Scopes variable -- inaccessible outside calcAge function
+
+const myName = 'Jonas';
+
+if (myName === 'Jonas') {
+  console.log(`Jonas is a ${job}`);
+  const age = 2037 - 1989;
+  var job = 'teacher';
+}
+
+function factorial(n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(5));
+
+console.log(me1); // var is hoisted and can be accessed before declaration
+// console.log(job1); // Reference error -- in temporal dead zone
+// console.log(year1); // reference error - in temporal dead zone
+
+var me1 = 'Jonas';
+let job1 = 'teacher';
+const year1 = 1999;
+
+// function hoisting
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+
+// Can only use function declarations before declaration
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArr = (a, b) => a + b;
+
+//  Example
+
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
