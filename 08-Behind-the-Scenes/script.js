38bbb3aa8108495c2868f00c37347d7838888a16
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-04-19 01:46:18 -0400

    Hoisting

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -87,3 +87,11 @@ var numProducts = 10;
 function deleteShoppingCart() {
   console.log('All products deleted!');
 }
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
