8931d1a9361ef45b0303b076808a056a3e0954e1
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-06 23:13:13 -0400

    Rest Operator

--- 09-Data-Structures-Operators/started.js
+++ 09-Data-Structures-Operators/started.js
@@ -33,6 +33,11 @@ const restaurant = {
     );
   },
 
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -74,6 +79,22 @@ const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
 
 // Methods
+const add = function (...numbers) {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    count += numbers[i];
+  }
+  console.log(`Sum of ${numbers} is ${count}.`);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 6];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
 /** SPREAD OPERATOR
  * -- Allows us to copy all or part of an interable into another array, object as individual elements
  * -- Unpacks an array
