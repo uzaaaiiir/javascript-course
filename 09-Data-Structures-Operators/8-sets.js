b0cc4b1f16998796522b4f70221aa4a9ef3b66b8
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-12 07:35:29 -0400

    Sets

---
+++ 09-Data-Structures-Operators/8-sets.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 1, time = '20:00', address }) {
+    console.log(
+      `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// SET -- unordered, unique sequence of items
+// -- contains no duplicates
+// -- no key value pairs, just items
+// -- also considered an iterable
+// -- cannot index over items in a set b/c the items are random and unordered
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Jonass'));
+
+// Methods of Sets
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+// Iterating over sets
+for (const order of ordersSet) {
+  console.log(order);
+}
+
+// Sets can be used to remove duplicate values of arrays
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffSet = [...new Set(staff)];
+console.log(staffSet);
+
+// Finding hte number of unique items in an iterable
+console.log(new Set('uzairmohiuddin').size);
