ea93956eaddbef70cab813f13f0d527749ee9ab1
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-12 08:36:59 -0400

    Coding Challenge #3

--- 09-Data-Structures-Operators/coding-challenge.js
+++ 09-Data-Structures-Operators/coding-challenge.js
@@ -65,7 +65,7 @@ console.log(game.printGoals(...game.scored));
 */
 
 // Coding Challenge 2
-
+/** 
 for (const [goal, name] of game.scored.entries()) {
   console.log(`Goal ${Number(goal) + 1}: ${name}`);
 }
@@ -79,4 +79,52 @@ console.log(`The average odd is ${count / 3}`);
 for (const [team, odds] of Object.entries(game.odds)) {
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odds of ${teamStr}: ${odds}`);
+}
+*/
+
+// CODING CHALLENGE 3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔄 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔄 Substitution'],
+  [64, '💛 Yellow Card'],
+  [69, '🔴 Red card'],
+  [70, '🔄 Substitution'],
+  [72, '🔄 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '💛 Yellow Card'],
+]);
+
+// 1. Create an array 'evemts' pf the differentgame events that happened [no duplicates]
+const eventsSet = new Set();
+for (const [time, event] of gameEvents) {
+  eventsSet.add(event);
+}
+const events = [...eventsSet];
+const events1 = [...new Set(gameEvents.values())];
+console.log(events);
+console.log(events1);
+
+// 2. Remove the yellow card at minute 64 from the game events log
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Print the following String to console:
+// "An event happened, on average, every 9 minutes"
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An events happened, on average, every ${time / gameEvents.size} minutes.`
+);
+
+// 4. Loop over events and log to console
+// Marking whether its in the first half or second half of the game [after 45 minutes]
+const HALF_TIME = 45;
+for (const [time, event] of gameEvents) {
+  console.log(
+    time <= HALF_TIME
+      ? `[FIRST HALF] ${time}: ${event}`
+      : `[SECOND HALF] ${time}: ${event}`
+  );
 }
