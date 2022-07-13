be78423f03bcc00bc67de67eeed679ea6fe0d02b
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-13 09:45:43 -0400

    String Coding Challenge

--- 09-Data-Structures-Operators/10-strings.js
+++ 09-Data-Structures-Operators/10-strings.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -171,3 +168,17 @@ const planesInLine = function (n) {
 };
 planesInLine(5);
 planesInLine(3);
+
+/////////////////////////
+// STRING EXTRA PRACTICE
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const flightArr = flights.split('+');
+console.log(flightArr);
+
+for (const flightItem of flightArr) {
+  const [title, to, from, time] = flightItem.split(';');
+  console.log(title, to, from, time);
+}
