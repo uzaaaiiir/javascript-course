08fdfd8f8c34b0e18d8f2a01230b6a82aa952819
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-13 15:19:38 -0400

    String Methods Coding Challenge

--- 09-Data-Structures-Operators/10-strings.js
+++ 09-Data-Structures-Operators/10-strings.js
@@ -175,10 +175,24 @@ planesInLine(3);
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const flightArr = flights.split('+');
-console.log(flightArr);
+/**
+ * 🔴 Delayed Departure from FAO to TXL (11h25)
+ *              Arrival from BRU to FAO (11h45)
+ *   🔴 Delayed Arrival from HEL to FAO (12h05)
+ *             Departure from FAO to LIS (12h30)
+ */
 
-for (const flightItem of flightArr) {
-  const [title, to, from, time] = flightItem.split(';');
-  console.log(title, to, from, time);
+const flightArr = flights.split('+');
+
+for (const [i, flightItem] of flightArr.entries()) {
+  let [title, from, to, time] = flightItem.split(';');
+
+  // title
+  title = title.replace('_', '');
+  title = title.startsWith('Delayed') ? `🔴 ${title.replace('_', ' ')}` : title;
+
+  const output = `${title} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(45);
+  console.log(output);
 }
