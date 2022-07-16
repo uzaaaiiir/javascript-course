d98297c0dbec0c3be1e07e53aee436d65bd8f501
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-07-15 22:23:10 -0400

    Default Parameters in JS

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+/** DEFAULT PARAMETERS
+ * -- if no parameters are passed, the unassigned parameters are going to be set to undefined
+ */
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5
+  numPassengers = numPassengers || 1;
+  price ||= 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking(5, 5, 5);
