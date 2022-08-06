98598be5031f8b3a9a1fccfe5f68c390f810461b
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-08-06 09:22:36 -0400

    Filter Array Method for withdrawals

--- 11-Arrays-Bankist/lecture-scripts/4-filter-method.js
+++ 11-Arrays-Bankist/lecture-scripts/4-filter-method.js
@@ -2,8 +2,11 @@
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const deposits = movements.filter(mov => mov > 0);
-console.log(deposits);
+const deposits = movements.filter(function (mov) {
+  return mov > 0; // returns positive numbers based on the condition
+});
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(deposits, withdrawals);
 
 const depositsFor = [];
 for (const mov of movements) {
