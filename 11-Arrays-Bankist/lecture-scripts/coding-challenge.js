e5b1f63ba3472772f02aa1d5287c5bfae28322f4
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-08-06 10:30:36 -0400

    Coding Challenge

--- 11-Arrays-Bankist/lecture-scripts/coding-challenge.js
+++ 11-Arrays-Bankist/lecture-scripts/coding-challenge.js
@@ -17,3 +17,22 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [9, 16, 6, 8, 3]);
 console.log('---------------------------------------');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// CODING CHALLENGE 2
+const testData1 = [5, 2, 4, 1, 15, 8, 3];
+const testData2 = [16, 6, 10, 5, 6, 1, 4];
+
+const averageHumanAgeOfDogs = function (dogAges) {
+  const humanAgeOfDogs = dogAges.map(dogAge =>
+    dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4
+  );
+  const adultDogs = humanAgeOfDogs.filter(dogAge => dogAge >= 18);
+
+  const average =
+    adultDogs.reduce((acc, dogAge) => acc + dogAge, 0) / adultDogs.length;
+
+  return average;
+};
+
+console.log(averageHumanAgeOfDogs(testData1));
+console.log(averageHumanAgeOfDogs(testData2));
