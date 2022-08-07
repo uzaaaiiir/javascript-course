4e0ddce6563ec195ea227fd42a8554c45f263039
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-08-07 09:09:01 -0400

    Closing Account

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -180,6 +180,26 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin = '';
+});
+
 displayMovements(account1.movements);
 createUsernames(accounts);
 /////////////////////////////////////////////////
