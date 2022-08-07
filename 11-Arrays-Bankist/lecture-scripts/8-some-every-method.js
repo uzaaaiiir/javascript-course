9d5a1cc3070337f4062cefc5dfe4e472329c871d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-08-07 09:31:48 -0400

    Implementing Bank Loans

--- 11-Arrays-Bankist/lecture-scripts/8-some-every-method.js
+++ 11-Arrays-Bankist/lecture-scripts/8-some-every-method.js
@@ -6,8 +6,21 @@ console.log(movements);
 // EQUALITY
 console.log(movements.includes(-130));
 
+// SOME METHOD
 // Checks if there exists any deposits that meet the condition
 const anyDeposits = movements.some(mov => mov > 0);
 const anyDepositsAbove3000 = movements.some(mov => mov > 3000);
 console.log(anyDeposits);
 console.log(anyDepositsAbove3000);
+
+// EVERY METHOD
+// Returns true if all elements in the array satisfy the condition
+const positiveMovements = [430, 1000, 700, 50, 90];
+console.log(movements.every(mov => mov > 0));
+console.log(positiveMovements.every(mov => mov > 0));
+
+// Separate Callback function
+const deposit = mov => mov > 0;
+console.log(positiveMovements.some(deposit));
+console.log(movements.every(deposit));
+console.log(positiveMovements.filter(deposit));
--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -184,6 +184,16 @@ btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
   const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
 });
 
 btnClose.addEventListener('click', function (e) {
