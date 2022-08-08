a667f108af1a8dab7cd81459dc199a1ea9ef266a
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2022-08-08 08:39:35 -0400

    Coding Challenge

--- 11-Arrays-Bankist/lecture-scripts/coding-challenge.js
+++ 11-Arrays-Bankist/lecture-scripts/coding-challenge.js
@@ -34,6 +34,7 @@ const averageHumanAgeOfDogs = function (dogAges) {
   return average;
 };
 
+// CODING CHALLENGE 3
 const averageHumanAgeOfDogs2 = function (dogAges) {
   return dogAges
     .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
@@ -45,3 +46,40 @@ console.log(averageHumanAgeOfDogs(testData1));
 console.log(averageHumanAgeOfDogs(testData2));
 console.log(averageHumanAgeOfDogs2(testData1));
 console.log(averageHumanAgeOfDogs2(testData2));
+
+// CODING CHALLENGE 4
+console.log('-------------------------------------------');
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// Adding Recommended Food portion
+dogs.forEach(dog => (dog.recommended = Math.pow(dog.weight, 0.75) * 28));
+console.log(dogs);
+
+// Find if Sarah's dog eats too much
+const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  sarahDog.curFood > sarahDog.recommended
+    ? 'Sarahs dog eats too much!'
+    : 'Sarahs dog eats too little!'
+);
+
+// 3.
+const { ownersEatTooMuch, ownersEatTooLittle } = dogs.reduce(
+  (owns, dog) => {
+    const eatsTooMuchOrLittle =
+      dog.curFood > dog.recommended ? 'ownersEatTooMuch' : 'ownersEatTooLittle';
+    owns[eatsTooMuchOrLittle].push(dog.owners);
+    owns[eatsTooMuchOrLittle] = owns[eatsTooMuchOrLittle].flat();
+    return owns;
+  },
+  { ownersEatTooMuch: [], ownersEatTooLittle: [] }
+);
+console.log(ownersEatTooMuch, ownersEatTooLittle);
+
+const eatsTooMuch = `dogs eat too much!`;
